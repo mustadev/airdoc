32792c4de197378ca6336ea010ff677a8a23e18b
mustadev <ouarrain.mustapha@gmail.com>
2020-03-25 22:14:07 +0100

    Revert "Revert "test sur update"" revert back to fix errors
    This reverts commit 604e6a459c27b747427e08dcc4dbbc8a0294c941.

--- airdoc/src/main/java/com/brainstormers/airdoc/controllers/CabinetController.java
+++ airdoc/src/main/java/com/brainstormers/airdoc/controllers/CabinetController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.management.OperationsException;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
