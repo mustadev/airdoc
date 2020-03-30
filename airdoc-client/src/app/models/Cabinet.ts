931a14fa66d9869a29299e22d04f4d48ca6a3535
mustadev <ouarrain.mustapha@gmail.com>
2020-03-30 03:35:18 +0200

    page de recherche

--- airdoc-client/src/app/models/Cabinet.ts
+++ airdoc-client/src/app/models/Cabinet.ts
@@ -1,3 +1,35 @@
+import { Review } from './Review';
+
 export class Cabinet {
-    
+    name:string;
+    speciality:string;
+    rating:number;
+    averageRating:number;
+    services:Array<string>;
+    reviews:Array<Review>;
+    city:string;
+    country:string;
+    minPrice:number;
+    maxPrice:number;
+
+    constructor(
+        name:string,
+        speciality:string, 
+        rating:number, 
+        averageRating:number, 
+        services:Array<string>, 
+        city:string, 
+        country:string,
+        minPrice:number,
+        maxPrice:number){
+            this.name= name;
+            this.speciality= speciality;
+            this.rating = rating;
+            this.averageRating = averageRating;
+            this.services = services;
+            this.city = city;
+            this.country = country;
+            this.minPrice = minPrice;
+            this.maxPrice = maxPrice;
+        };
 }
