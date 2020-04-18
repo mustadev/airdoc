e300b31374cad49e95415fe8aac31e8f8b09089b
mustadev <ouarrain.mustapha@gmail.com>
2020-04-18 23:27:58 +0200

    belaid contact and about pages merged

--- airdoc-client/src/app/components/pages/contact/contact.component.ts
+++ airdoc-client/src/app/components/pages/contact/contact.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Contact } from 'src/app/models/Contact';
-import { ContactService } from 'src/app/services/contact.service';
+import { ContactService } from './../../../services/contact.service';
 import { Router } from '@angular/router';
+import { Contact } from './../../../models/Contact';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -11,17 +11,17 @@ import { Router } from '@angular/router';
 export class ContactComponent implements OnInit {
 
   contact:Contact = new Contact();
+  submitted=false;
+
   constructor(
     private contactService:ContactService,
     private router:Router) { }
 
   ngOnInit(): void {
   }
-  //TODO BELAID 
-  //sawb form 
-  // t3mert this.contact s value n input li dark
-  // 3ad execute lfonctionad
+ 
   onSubmit(){
+    this.submitted = true;
     console.log("submiting contact Contact ", JSON.stringify(this.contact));
     this.contactService.addContact(this.contact).subscribe(res => {
       console.log("submiting contact Contact ", JSON.stringify(res));
